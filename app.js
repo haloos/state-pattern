ba2bce80ad7fd79e40a701c0d33b2b958810cacc
Jason <haloos@me.com>
2019-01-27 10:29:41 -0500

    home, form, contact forms

--- app.js
+++ app.js
@@ -25,4 +25,60 @@ const homeState = function(page) {
 };
 
 // About State
+const aboutState = function(page) {
+  document.querySelector('#heading').textContent = 'About Us';
+  document.querySelector('#content').innerHTML =`
+    <p>This is the about page</p>
+  `;
+};
 
+// Contact State
+const contactState = function(page) {
+  document.querySelector('#heading').textContent = 'Contact Us';
+  document.querySelector('#content').innerHTML =`
+    <form>
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control"> 
+      </div> 
+      <div class="form-group"> 
+      <label>Email address</label> 
+      <input type="email" class="form-control"> 
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `;
+};
+
+// Instantiate pageState
+
+const page = new PageState();
+
+// Init the first state
+page.init();
+
+// UI Vars
+const home = document.getElementById('home'),
+      about = document.getElementById('about'),
+      constact = document.getElementById('contact');
+
+// Home
+home.addEventListener('click', (e) => {
+  page.change(new homeState);
+
+  e.preventDefault();
+});
+
+// About
+about.addEventListener('click', (e) => {
+  page.change(new aboutState);
+
+  e.preventDefault();
+});
+
+// Contact
+contact.addEventListener('click', (e) => {
+  page.change(new contactState);
+
+  e.preventDefault();
+});
